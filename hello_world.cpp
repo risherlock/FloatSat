c25b61422ebc3ef242353a365f3b29b3403f5abe
sriza <sriza054@gmail.com>
2024-10-16 18:57:20 +0200

    add counter and decrease print frequency

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,11 +2,13 @@
 
 class HelloWorld : public StaticThread<>
 {
+  uint32_t count = 0;
+
   void run()
   {
-    TIME_LOOP(100 * MILLISECONDS, 100 * MILLISECONDS)
+    TIME_LOOP(100 * MILLISECONDS, 1000 * MILLISECONDS)
     {
-      PRINTF("Hello World!\n");
+      PRINTF("%ld: Hello World!\n", count++);
     }
   }
 } hello_world;
